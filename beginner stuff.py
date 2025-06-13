43cb2febeca22cb781379446311f35abd266d76e
nganesh <nganesh@iu.edu>
2025-06-12 19:29:20 -0700

    beginner lists

---
+++ beginner stuff.py
@@ -0,0 +1,16 @@
+# empty list
+list = []
+print()
+
+# list of numbers
+numbers = [1, 2, 3, 4, 5, 6, 7]
+print(numbers)
+
+# * operator
+quadruple = [1, 2, 3, 4] * 4
+print(quadruple)
+
+# reversing
+reverse = [10, 20, 30, 40, 50]
+reverse = reverse[::-1]
+print(reverse)
