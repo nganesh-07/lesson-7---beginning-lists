6e8ab6f9f2bcb450f268d7074cd72f2d020f31dd
nganesh <nganesh@iu.edu>
2025-06-23 19:09:12 -0700

    project 1

---
+++ project 1.py
@@ -0,0 +1,11 @@
+numbers = [1,9,3,8,"italy",7,"switzerland",64, 5]
+first = numbers[0]
+second = numbers[-1]
+
+def square(num1):
+    squaring = num1**2
+    return squaring
+
+
+print(square(first))
+print(square(second))
